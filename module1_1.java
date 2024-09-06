644a211af8834c4b9d39352bd5fd39c6c29a2fca
Johnboy415 <jespinosa26@hawkmail.hccfl.edu>
2024-09-06 17:37:38 -0400

    Update module1_1.java

--- module1_1.java
+++ module1_1.java
@@ -1,5 +1,6 @@
 public class module1_1 {
     public static void main(String[] args) {
+        System.out.println("2024/09/04_EspinosaJonathan_Exercise1.1");
         //Display message "Welcome to Java", "Welcome to Computer Science", "Programming is fun"
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
