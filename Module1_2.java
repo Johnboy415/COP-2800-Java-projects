f2feeb895e71fb2c0ecfdb16b5011cf1c5c904e4
Johnboy415 <jespinosa26@hawkmail.hccfl.edu>
2024-09-06 18:44:00 -0400

    Add files via upload

---
+++ Module1_2.java
@@ -0,0 +1,11 @@
+public class Module1_2 {
+    public static void main(String[] args) {
+        System.out.println("2024/09/04_EspinosaJonathan_Exercise1.1");
+        //Display message welocme to Java 5 times        
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
