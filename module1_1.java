335f6def3526bdeb7c2172b9ae88ce0d2146f2e6
Johnboy415 <jespinosa26@hawkmail.hccfl.edu>
2024-09-06 16:42:00 -0400

    Add files via upload

---
+++ module1_1.java
@@ -0,0 +1,8 @@
+public class module1_1 {
+    public static void main(String[] args) {
+        //Display message "Welcome to Java", "Welcome to Computer Science", "Programming is fun"
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
